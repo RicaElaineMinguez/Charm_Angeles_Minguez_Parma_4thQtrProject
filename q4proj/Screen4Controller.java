be6d6b8c513010518eaf669b7787293b5fb18a1e
RicaElaineMinguez <124059699+RicaElaineMinguez@users.noreply.github.com>
2023-05-05 09:14:32 +0800

    Update Screen4Controller.java

--- q4proj/Screen4Controller.java
+++ q4proj/Screen4Controller.java
@@ -31,38 +31,38 @@ import q4proj.Model.*;
  * @author briep
  */
 public class Screen4Controller implements Initializable {
-    
+
     @FXML private Button back;
     @FXML public GridPane board;
 //    private ImageView[][] realBoard = new ImageView[8][8];
     private ArrayList<ImageView> realBoard = new ArrayList(64);
-    
+
     @FXML
     private void backWindow(ActionEvent event)throws IOException{
         Node node = (Node) event.getSource();
         Scene currentScene = node.getScene();
         Stage currentStage = (Stage) currentScene.getWindow();
-        
+
         Parent root = FXMLLoader.load(getClass().getResource("Screen1.fxml"));
         Scene playScene = new Scene(root);
         currentStage.hide();
         currentStage.setScene(playScene);
         currentStage.show();   
     }
-    
+
     @FXML
     private void Screen5Window(ActionEvent event)throws IOException{
         Node node = (Node) event.getSource();
         Scene currentScene = node.getScene();
         Stage currentStage = (Stage) currentScene.getWindow();
-        
+
         Parent root = FXMLLoader.load(getClass().getResource("Screen5.fxml"));
         Scene playScene = new Scene(root);
         currentStage.hide();
         currentStage.setScene(playScene);
         currentStage.show();   
     }
-    
+
 //    @FXML
 //    private void clickCell(){
 //        board.setOnMouseClicked(event -> {
@@ -83,33 +83,73 @@ public class Screen4Controller implements Initializable {
             Image icon = new Image("q4proj/circle1.png");
             source.setImage(icon);
     }
-    
+
     @FXML
     public void makeBoard() {
-        
+
         for(int i=0; i<64; i++){
-            ImageView icon = new ImageView();
+            ImageView icon = new ImageView(new Image(getClass().getResourceAsStream("emptytile.png")));
+            icon.setFitHeight(30);
+            icon.setFitWidth(30);
             realBoard.add(icon);
             int row = i/8;
             int col = i%8;
             board.add(icon,col,row);
-            
-            
-            
+            System.out.println("create");
+
+
+
             icon.setOnMouseClicked(event -> {
+                System.out.println("click");
                 ImageView source = (ImageView) event.getTarget();
                 int imageNum = realBoard.indexOf(source);
                 int x = imageNum/8;
                 int y = imageNum%8;
+                Player playerturn;
+                
                 if(Tile.roundNum%2 == 1) {
-                    Tile.play(x,y, Screen3Controller.player1);
+                    playerturn = Player.playerList.get(0);   
                 }
-                else Tile.play(x,y, Screen3Controller.player2);
+                else {
+                    playerturn = Player.playerList.get(1);   
+                }
+                
+//                String filename = "q4proj/"+playerturn.getShape()+"1.png";
+//                Image tileicon = new Image(filename);
+//                source.setImage(tileicon);
+//                
+//                Tile.incRoundNum();
+//                
+//                int tilePopulation = Tile.grid[x][y].getNumberStatus();
+//                System.out.println(tilePopulation);
+                
+                int tilePopulation = Tile.grid[x][y].getNumberStatus();
+                System.out.println(tilePopulation);
+                String filename = null;
+                
+                if(tilePopulation==0) filename = "q4proj/"+playerturn.getShape()+"1.png";
+                else if(tilePopulation==1) filename = "q4proj/"+playerturn.getShape()+"2.png";
+                else if(tilePopulation==2 && playerturn.getShape()!="triangle") filename = "q4proj/"+playerturn.getShape()+"3.png";
+                
+                Image tileicon = new Image(filename);
+                source.setImage(tileicon);
+                
+                Tile.incRoundNum();
+                Tile.grid[x][y].incNumberSatus();
+//                
+                
+//            int columnIndex = GridPane.getColumnIndex(source);
+//            int rowIndex = GridPane.getRowIndex(source);
+//                Player x = Player.playerList.get(0);
+//                String filename = "q4proj/"+x.getShape()+"1.png";
+//                System.out.println(filename);
+//                Image tileicon = new Image(filename);
+//                source.setImage(tileicon);
             });
         }
-        
+
 //    ImageView[][] imageGrid = new ImageView[8][8];
-    
+
 //    for(int x = 0; x < 8; x++) {
 //      for(int y = 0; y < 8; y++) {
 //        
@@ -132,8 +172,9 @@ public class Screen4Controller implements Initializable {
 //    @Override
     public void initialize(URL url, ResourceBundle rb) {
         makeBoard();
+        Tile.makeGrid();
     }    
-    
+
 }
 
 //generate for loop in a for loop to generate the 8x8 gridpane with imageviews
