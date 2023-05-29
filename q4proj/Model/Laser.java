0c38cb5c7959f05cfb4c616d70089de4d85f32ca
RicaElaineMinguez <124059699+RicaElaineMinguez@users.noreply.github.com>
2023-05-29 22:09:44 +0800

    Add files via upload

--- q4proj/Model/Laser.java
+++ q4proj/Model/Laser.java
@@ -1,16 +1,26 @@
 package q4proj.Model;
 import java.util.ArrayList;
+import javafx.scene.image.ImageView;
 
 public abstract class Laser extends Obstacle{
   
   protected static int[] trajectory = {0, 1, 2, 3, 4, 5, 6, 7};
   public static ArrayList<Laser> laserList = new ArrayList<>();
+  public String status;
+  public ImageView space;
   
   public abstract void erase(); //to be called when the laser is clicked
   
   public void shift(){
     this.location++;
     this.location %= 7;
+  }
+  
+  public void activate(){
+      this.status = "active";
+  }
+  public void deactivate(){
+      this.status = "inactive";
   }
   
 }
--- q4proj/Model/Player.java
+++ q4proj/Model/Player.java
@@ -1,5 +1,6 @@
 package q4proj.Model;
 import java.util.ArrayList;
+import javafx.scene.image.Image;
 
 public class Player {
   protected String name;
@@ -63,12 +64,33 @@ public class Player {
     this.criticalMass = c;
   }
   
-  public void click(int x, int y) {
-    Tile.play(x, y, this);
-    Tile.nextTurn();
-  }
-  
+//  public void click(int x, int y) {
+//    Tile.play(x, y, this);
+//    Tile.nextTurn();
+//  }
+//  
   public void disperse(int x, int y){
       
+  }
+  
+  public void updateTileView(int x, int y){
+    if(Tile.grid[x][y].numberStatus>0 && !(Tile.grid[x][y].playerStatus.equals("empty"))){
+        String filename = "q4proj/" + Tile.grid[x][y].playerStatus + Tile.grid[x][y].numberStatus + ".png";
+        Image icon = new Image(filename);
+        Tile.grid[x][y].tileView.setImage(icon);
+    }
+    else{
+        String filename = "q4proj/emptytile.png";
+        Image icon = new Image(filename);
+        Tile.grid[x][y].tileView.setImage(icon);
+    } 
+  }
+  
+  public void updateIfTileExists(int x, int y, String shape){
+      if((x>=0 && x<8) && (y>=0 && y<8)){
+          Tile.grid[x][y].setPlayerStatus(shape);
+          Tile.grid[x][y].setNumberStatus(Tile.grid[x][y].getNumberStatus() + 1);
+          updateTileView(x,y);
+      }
   }
 }
